897a8a8690905f788f7a1e2ace05cbf9906f2f0f
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-13 18:01:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
 if phrase=="I LOVE YOU GRANDMA!"
   print "I LOVE YOU TOO PUMPKIN!"
 elsif phrase==phrase.upcase
-  print "NO, NOT SINCE 1938!"
+  print "NO,NOT SINCE 1938!"
 else
   print "HUH?! SPEAK UP,SONNY!"
 end
